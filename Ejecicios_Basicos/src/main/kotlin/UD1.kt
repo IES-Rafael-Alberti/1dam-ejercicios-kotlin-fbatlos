b0e5438ffb2991a1aea286822948d0dcae2b126c
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2023-12-12 18:32:02 +0100

    U-1

--- Ejecicios_Basicos/src/main/kotlin/UD1.kt
+++ Ejecicios_Basicos/src/main/kotlin/UD1.kt
@@ -1,10 +1,50 @@
+import java.util.*
 import kotlin.math.ceil
 
 fun pedirnumero(): Float {
     val num = readln().toFloat()
     return num
 }
+fun ej18(){
+    println("Dame tu nombre :")
+    val nombre_completo = readln()
+    println(nombre_completo.uppercase())
+    println(nombre_completo.lowercase())
+    val nombre= nombre_completo.split(" ").joinToString(" ").replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+    println(nombre)
+}
 
+
+
+
+/**
+ * La funcion ej15 pedirá tu capital , que llamará a la funcion pedirnumero y esta
+ * entrará en un while que calculará el interes anual del 4% de la cantidad añadida
+ * durante 3 años.
+ */
+fun ej15() = try {
+    println("Dime tu capital para el interes: ")
+    var capital = pedirnumero()
+    var año = 0
+    do {
+        val interes = capital*0.04
+        capital = (capital+interes).toFloat()
+        año++
+        println("Tú capital tras $año º año es de ${ceil(capital)}")
+    }while (año!=3)
+}
+catch (e: NumberFormatException) {
+    println("**ERROR** $e")
+} catch (e: Exception) {
+    print("**ERROR** $e")
+}
+
+
+
+/**
+ * La funcion ej12 llamará a pedirnumero dos veces para saber su peso y su altura ,
+ * en el print hacemos la operacion necesaria y la redondeamos hacia arriba.
+ */
 fun ej12(){
     try {
         println("Dime un tu peso en Kg:")
@@ -13,7 +53,11 @@ fun ej12(){
         val altura =pedirnumero()
         println("Tu indice de masa corporal es ${ceil(peso/(altura*altura))}")
     }
-
+    catch (e: NumberFormatException) {
+        println("**ERROR** $e")
+    } catch (e: Exception) {
+        print("**ERROR** $e")
+    }
 }
 
 
