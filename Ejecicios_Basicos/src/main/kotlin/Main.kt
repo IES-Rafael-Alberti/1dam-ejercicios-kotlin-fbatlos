1693c04c026b4610a2b2bfa75d91db1a4a6c2257
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-01-16 12:34:19 +0100

    dios salvame

--- Ejecicios_Basicos/src/main/kotlin/Main.kt
+++ Ejecicios_Basicos/src/main/kotlin/Main.kt
@@ -1,13 +1,5 @@
 fun main() {
-    //ej4()
-    //ej6()
-    //ej12()
-    //ej15()
-    //ej18()
-    //ej21()
-    //ej22()
-    //ej24()
-    //ej25()
+
     //ej2C2()
     //ej3C2()
     //ej6C2()
@@ -19,10 +11,7 @@ fun main() {
     //ej13I2()
     //ej15I2()
     //ej19I2()
-    ej25I2()
+    //ej25I2()
+
 }
-fun limpiaConsola() {
-for (i in 1..10) {
- println()
- }
-}
+
